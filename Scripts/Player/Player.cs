e04e26aa7c61d59cd781d95e8dc056414569e547
konstantin-oskolkov <konstantin-oskolkov@yandex.ru>
2022-12-12 10:55:23 +0300

    ver_0.22

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -1,47 +1,21 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Rigidbody2D))]
-
 
 public class Player : MonoBehaviour
 {
    [SerializeField] private int _health;
-   [SerializeField] private float _moveSpeed;
-   [SerializeField] private float _jumpForce;
-   [SerializeField] private float _checkRadius;
-   [SerializeField] private Transform _checkGround;
-   [SerializeField] private LayerMask _ground;
 
-   private Animator _animator;
    private SpriteRenderer _sprite;
-   private Rigidbody2D _rigitBody;
-   private Vector3 _vector;
-   private bool _isOnGround;
    private int _currentHealth;
-
-   private const string OnGround = "onGround";
-   private const string MoveHorisontal = "moveHorisontal";
-   private const string Horizontal = "Horizontal";
 
    public event Action<int, int> HealthChanged;
 
    private void Start()
    {
-      _animator = GetComponent<Animator>();
       _sprite = GetComponent<SpriteRenderer>();
-      _rigitBody = GetComponent<Rigidbody2D>();
       _currentCoin = 0;
-   }
-
-   private void Update()
-   {
-      Run();
-      Flip();
-      _isOnGround = IsOnGround();
-      Jump();
    }
 
    public void Damage(int damage)
@@ -63,24 +37,4 @@ public class Player : MonoBehaviour
 
       HealthChanged?.Invoke(_currentHealth, _health);
    }
-
-   private void Run()
-   {
-      _vector = new Vector3(Input.GetAxis(Horizontal), 0);
-      transform.position += _vector * _moveSpeed * Time.deltaTime;
-      _animator.SetFloat(MoveHorisontal, Mathf.Abs(_vector.x));
-   }
-
-   private void Jump()
-   {
-         _animator.SetBool(OnGround, _isOnGround);
-
-      if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
-         _rigitBody.velocity = new Vector2(_rigitBody.velocity.x, _jumpForce);
-   }
-
-   private bool IsOnGround() => Physics2D.OverlapCircle(_checkGround.position, _checkRadius, _ground);
-
-   private void Flip() => _sprite.flipX = _vector.x < 0;
-
 }
